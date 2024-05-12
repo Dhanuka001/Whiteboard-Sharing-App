791492bd2ce9cf007bef1d21a2cdda08f776e3b2
Dhanuka001 <137430860+Dhanuka001@users.noreply.github.com>
2024-05-13 00:47:57 +0530

    did some form changes

--- frontend/src/Components/Forms/Index.jsx
+++ frontend/src/Components/Forms/Index.jsx
@@ -5,7 +5,7 @@ import JoinRoomForm from "./JoinRoomForm/Index";
 const Forms = ()=> {
     return (
         <div className="row h-100 pt-5">
-            <div className="col-md-4 mt-5 py-3 p-4 form-box border border-2 border-primary rounded-3 mx-auto d-flex flex-column align-items-center">
+            <div className="col-md-4 mt-5 py-3 p-4 form-box border border-2 border-primary rounded-3 mx-auto d-flex flex-column align-items-center ">
                 <h1 className="text-primary fw-bold">Create Room</h1> 
                 <CreateRoomForm/>
             </div>
